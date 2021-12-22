85e84d62acce8276e65d788b2543826f348ffabd
gersonvinicius <vini_gerson@hotmail.com>
2021-12-22 01:15:03 -0300

    Teste finalizado

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -21,6 +21,19 @@ class ProductStructure
     public function make(): array
     {
        //todo your code.
-        return [];
+       $array = array();
+       $produtos = self::products;
+       foreach($produtos as $product){
+           $p = explode('-',$product);
+           $counts = array_count_values($produtos);
+           $soma = $counts[$product];
+           if(in_array($p[0], $array))
+               $array[$p[0]] = array_merge($array[$p[0]], array($p[1] => $soma));
+           else
+               $array[$p[0]][$p[1]] = $soma;
+           unset($produtos[$p[0]]);
+       }
+       // echo '<pre>'; print_r($array); echo '</pre>';
+       return $array;
     }
 }
